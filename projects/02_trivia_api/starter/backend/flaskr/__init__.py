05cf2c98243ab0ab6e1a9c559b52bcfa69474b58
Neetha Jain <neethajain@Neethas-MacBook-Air.local>
2020-11-05 19:25:32 -0800

    Fixed delete API

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -102,21 +102,22 @@ def create_app(test_config=None):
   @app.route('/questions/<int:question_id>', methods=['DELETE'])
   def delete_question(question_id):
     try:
-        question = Question.query(Question.id == question_id).one_or_none()
-        if question is None:
-            abort(404)
+      question = Question.query.filter(Question.id == question_id).one_or_none()
+      print("delete_question 2")
+      if question is None:
+          abort(404)
 
-        question.delete()
+      question.delete()
 
-        all_questions = Question.query.order_by(Question.id).all()
-        current_questions = paginate_questions(request, all_questions)
-    
-        return jsonify({
-            'success': True,
-            'deleted': question_id,
-            'questions': current_questions,
-            'total_questions': len(all_questions)
-        })
+      all_questions = Question.query.order_by(Question.id).all()
+      current_questions = paginate_questions(request, all_questions)
+  
+      return jsonify({
+          'success': True,
+          'deleted': question_id,
+          'questions': current_questions,
+          'total_questions': len(all_questions)
+      })
 
     except Exception as e:
       print(e)
